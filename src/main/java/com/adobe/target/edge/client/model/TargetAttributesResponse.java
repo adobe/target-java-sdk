af6b178da55fb7465c905be39d49787218032a8f
artur-ciocanu <artur.ciocanu@gmail.com>
2020-11-04 12:02:22 -0800

    TNT-38893 - ensure attributes response is unmodifiable (#19)
    * TNT-38893 - ensure attributes response is unmodifiable
    
    Co-authored-by: Artur Ciocanu <ciocanu@adobe.com>

--- src/main/java/com/adobe/target/edge/client/model/TargetAttributesResponse.java
+++ src/main/java/com/adobe/target/edge/client/model/TargetAttributesResponse.java
@@ -11,69 +11,100 @@
  */
 package com.adobe.target.edge.client.model;
 
-import com.adobe.target.delivery.v1.model.*;
+import com.adobe.target.delivery.v1.model.DeliveryResponse;
+import com.adobe.target.delivery.v1.model.ExecuteResponse;
+import com.adobe.target.delivery.v1.model.MboxResponse;
+import com.adobe.target.delivery.v1.model.Option;
+import com.adobe.target.delivery.v1.model.OptionType;
+import com.adobe.target.delivery.v1.model.PageLoadResponse;
+import com.adobe.target.delivery.v1.model.PrefetchResponse;
 import com.adobe.target.edge.client.Attributes;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TargetAttributesResponse implements Attributes {
 
+  private static final String GLOBAL_MBOX = "target-global-mbox";
+
   private final TargetDeliveryResponse response;
-  private final Map<String, Map<String, Object>> content = new HashMap<>();
-  private volatile boolean contentCreated = false;
+  private final Map<String, Map<String, Object>> content;
 
   public TargetAttributesResponse(TargetDeliveryResponse response) {
     this.response = response;
+    this.content = toMap(response);
   }
 
   @Override
   public Boolean getBoolean(String mbox, String key, Boolean defaultValue) {
     Map<String, Object> map = toMboxMap(mbox);
-    if (map != null) {
-      Object value = map.get(key);
-      if (value instanceof Boolean) {
-        return ((Boolean) value);
-      }
+
+    if (map == null) {
+      return defaultValue;
     }
+
+    Object value = map.get(key);
+
+    if (value instanceof Boolean) {
+      return ((Boolean) value);
+    }
+
     return defaultValue;
   }
 
   @Override
   public String getString(String mbox, String key) {
     Map<String, Object> map = toMboxMap(mbox);
-    if (map != null) {
-      Object value = map.get(key);
-      if (value != null) {
-        return value.toString();
-      }
+
+    if (map == null) {
+      return null;
     }
+
+    Object value = map.get(key);
+
+    if (value != null) {
+      return value.toString();
+    }
+
     return null;
   }
 
   @Override
   public Integer getInteger(String mbox, String key, Integer defaultValue) {
     Map<String, Object> map = toMboxMap(mbox);
-    if (map != null) {
-      Object value = map.get(key);
-      if (value instanceof Number) {
-        return ((Number) value).intValue();
-      } else if (value instanceof String) {
-        return Double.valueOf((String) value).intValue();
-      }
+
+    if (map == null) {
+      return defaultValue;
     }
+
+    Object value = map.get(key);
+
+    if (value instanceof Number) {
+      return ((Number) value).intValue();
+    } else if (value instanceof String) {
+      return Double.valueOf((String) value).intValue();
+    }
+
     return defaultValue;
   }
 
   @Override
   public Double getDouble(String mbox, String key, Double defaultValue) {
     Map<String, Object> map = toMboxMap(mbox);
-    if (map != null) {
-      Object value = map.get(key);
-      if (value instanceof Number) {
-        return ((Number) value).doubleValue();
-      } else if (value instanceof String) {
-        return Double.parseDouble((String) value);
-      }
+
+    if (map == null) {
+      return defaultValue;
     }
+
+    Object value = map.get(key);
+
+    if (value instanceof Number) {
+      return ((Number) value).doubleValue();
+    } else if (value instanceof String) {
+      return Double.parseDouble((String) value);
+    }
+
     return defaultValue;
   }
 
@@ -84,80 +115,107 @@ public class TargetAttributesResponse implements Attributes {
 
   @Override
   public Map<String, Object> toMboxMap(String mbox) {
-    Map<String, Object> mboxContent = this.content.get(mbox);
-    if (mboxContent != null) {
-      return mboxContent;
-    }
-    synchronized (this.content) {
-      mboxContent = this.content.get(mbox);
-      if (mboxContent != null) {
-        return mboxContent;
-      }
-      return toMap().get(mbox);
-    }
+    return this.content.get(mbox);
   }
 
   @Override
   public Map<String, Map<String, Object>> toMap() {
-    if (this.response == null) {
-      return null;
-    }
-    if (this.contentCreated) {
-      return this.content;
-    }
-    synchronized (this.content) {
-      String globalMbox = this.response.getResponseStatus().getGlobalMbox();
-      if (globalMbox == null) {
-        globalMbox = "target-global-mbox";
-      }
-      DeliveryResponse response = this.response.getResponse();
-      PrefetchResponse prefetchResponse = response.getPrefetch();
-      if (prefetchResponse != null) {
-        PageLoadResponse pageloadResponse = prefetchResponse.getPageLoad();
-        if (pageloadResponse != null) {
-          addOptions(pageloadResponse.getOptions(), globalMbox);
-        }
-        List<PrefetchMboxResponse> mboxes = prefetchResponse.getMboxes();
-        if (mboxes != null) {
-          for (int i = mboxes.size() - 1; i >= 0; i--) {
-            MboxResponse resp = mboxes.get(i);
-            String mbox = resp.getName();
-            List<Option> options = resp.getOptions();
-            addOptions(options, mbox);
-          }
-        }
-      }
-      ExecuteResponse executeResponse = response.getExecute();
-      if (executeResponse != null) {
-        PageLoadResponse pageloadResponse = executeResponse.getPageLoad();
-        if (pageloadResponse != null) {
-          addOptions(pageloadResponse.getOptions(), globalMbox);
-        }
-        List<MboxResponse> mboxes = executeResponse.getMboxes();
-        if (mboxes != null) {
-          for (int i = mboxes.size() - 1; i >= 0; i--) {
-            MboxResponse resp = mboxes.get(i);
-            String mbox = resp.getName();
-            List<Option> options = resp.getOptions();
-            addOptions(options, mbox);
-          }
-        }
-      }
-      this.contentCreated = true;
-    }
     return this.content;
   }
 
-  private void addOptions(List<Option> options, String mbox) {
-    Map<String, Object> mboxContent = this.content.computeIfAbsent(mbox, k -> new HashMap<>());
+  private static Map<String, Map<String, Object>> toMap(TargetDeliveryResponse targetResponse) {
+    if (targetResponse == null) {
+      return null;
+    }
+
+    Map<String, Map<String, Object>> result = new HashMap<>();
+    String globalMbox = getGlobalMbox(targetResponse);
+    DeliveryResponse response = targetResponse.getResponse();
+    PrefetchResponse prefetchResponse = response.getPrefetch();
+    ExecuteResponse executeResponse = response.getExecute();
+
+    processPrefetch(result, prefetchResponse, globalMbox);
+    processExecute(result, executeResponse, globalMbox);
+
+    return toReadOnlyMap(result);
+  }
+
+  private static String getGlobalMbox(TargetDeliveryResponse response) {
+    String globalMbox = response.getResponseStatus().getGlobalMbox();
+
+    return globalMbox == null ? GLOBAL_MBOX : globalMbox;
+  }
+
+  private static void processPrefetch(
+      Map<String, Map<String, Object>> accumulator, PrefetchResponse response, String globalMbox) {
+    if (response == null) {
+      return;
+    }
+
+    processPageLoad(accumulator, response.getPageLoad(), globalMbox);
+    processMboxes(accumulator, response.getMboxes());
+  }
+
+  private static void processExecute(
+      Map<String, Map<String, Object>> accumulator, ExecuteResponse response, String globalMbox) {
+    if (response == null) {
+      return;
+    }
+
+    processPageLoad(accumulator, response.getPageLoad(), globalMbox);
+    processMboxes(accumulator, response.getMboxes());
+  }
+
+  private static void processPageLoad(
+      Map<String, Map<String, Object>> accumulator, PageLoadResponse response, String globalMbox) {
+    if (response == null) {
+      return;
+    }
+
+    addOptions(accumulator, response.getOptions(), globalMbox);
+  }
+
+  private static <T extends MboxResponse> void processMboxes(
+      Map<String, Map<String, Object>> accumulator, List<T> mboxes) {
+    if (mboxes == null) {
+      return;
+    }
+
+    for (int i = mboxes.size() - 1; i >= 0; i--) {
+      MboxResponse resp = mboxes.get(i);
+      String mbox = resp.getName();
+      List<Option> options = resp.getOptions();
+
+      addOptions(accumulator, options, mbox);
+    }
+  }
+
+  private static void addOptions(
+      Map<String, Map<String, Object>> accumulator, List<Option> options, String mbox) {
+    Map<String, Object> mboxContent = accumulator.computeIfAbsent(mbox, k -> new HashMap<>());
+
     for (int j = options.size() - 1; j >= 0; j--) {
       Option option = options.get(j);
-      Object contentMap = option.getContent();
-      if (option.getType() == OptionType.JSON && contentMap instanceof Map) {
+
+      if (isJsonOption(option)) {
+        Object contentMap = option.getContent();
         @SuppressWarnings("unchecked")
         Map<String, Object> contentObj = (Map<String, Object>) contentMap;
         mboxContent.putAll(contentObj);
       }
     }
+  }
+
+  private static boolean isJsonOption(Option option) {
+    Object contentMap = option.getContent();
+    return option.getType() == OptionType.JSON && contentMap instanceof Map;
+  }
+
+  private static Map<String, Map<String, Object>> toReadOnlyMap(Map<String, Map<String, Object>> map) {
+    Map<String, Map<String, Object>> result = new HashMap<>(map.size());
+
+    map.keySet().forEach(key -> result.put(key, Collections.unmodifiableMap(map.get(key))));
+
+    return Collections.unmodifiableMap(result);
   }
 }
