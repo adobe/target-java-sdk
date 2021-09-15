90d586887aac51fe161912734d6c180dbfa1d9e9
Don C <cottingh@adobe.com>
2021-09-15 15:54:44 -0700

    update codegen script to use maven wrapper

--- codegeneration/codegen.sh
+++ codegeneration/codegen.sh
@@ -6,7 +6,7 @@ git clone git@github.com:dcottingham/openapi-generator.git
 cd openapi-generator
 git fetch --tags
 git checkout tags/v5.2.0_custom
-mvn clean install package -Dmaven.javadoc.skip=true -Dmaven.test.skip=true
+./mvnw clean install package -Dmaven.javadoc.skip=true -Dmaven.test.skip=true
 java -jar modules/openapi-generator-cli/target/openapi-generator-cli.jar generate -g java -c ../config.json -i ../../openapi/delivery/api.yaml -o ../build -t ../template --skip-validate-spec
 mkdir ../../src/main/java/com/adobe/target/delivery/v1/model/
 cp ../build/src/main/java/com/adobe/target/delivery/v1/model/* ../../src/main/java/com/adobe/target/delivery/v1/model/
